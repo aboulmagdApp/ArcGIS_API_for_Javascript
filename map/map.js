6b67b40c0eef2af052bdc53f828642cbc1b20db9
aboulmagdApp <aboulmagd@live.com>
2019-05-23 16:06:19 +0300

    Working with the Map View and Scene View

--- map/map.js
+++ map/map.js
@@ -1,16 +1,38 @@
-let map1;
+let topomap;
+let satmap;
+let streetsmap;
 let mapview;
 require([
     "esri/Map",
-    "esri/views/MapView"
+    "esri/views/MapView",
+    "esri/views/SceneView"
 ], function (
     Map,
-    MapView
+    MapView,
+    SceneView
 ) {
-      map1 = new Map({ basemap:"topo"});
-      mapview = new MapView({ 
-          container: "mapDiv", 
-          map: map1 ,zoom:11,
-          center:[46.733201068855806,24.714444348574713] //this center for Riyadh
+        topomap = new Map({ basemap: "topo" });
+        satmap = new Map({ basemap: "satellite" });
+        streetsmap = new Map({ basemap: "streets" });
+        let viewoptions = {
+            container: "mapDiv",
+            map: streetsmap,
+            zoom: 11,
+            center: [46.733201068855806, 24.714444348574713] //this center for Riyadh
+        }
+        // mapview = new MapView({
+        //     container: "mapDiv",
+        //     map: map1, zoom: 11,
+        //     center: [46.733201068855806, 24.714444348574713] //this center for Riyadh
+        // });
+        sceneView = new SceneView(viewoptions);
+        //using arrow function
+        document.getElementById("topo").addEventListener("click",
+            () => { sceneView.map = topomap });
+        document.getElementById("satellite").addEventListener("click", function () {
+            sceneView.map = satmap;
+        });
+        document.getElementById("streets").addEventListener("click", function () {
+            sceneView.map = streetsmap;
         });
     });
