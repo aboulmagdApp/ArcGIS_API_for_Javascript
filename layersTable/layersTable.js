edfbbc6232af01bdd33c96c01983cfcf8eb4aee9
aboulmagdApp <aboulmagd@live.com>
2019-05-26 00:29:16 +0300

    Code cleanup

--- layersTable/layersTable.js
+++ layersTable/layersTable.js
@@ -1,25 +1,7 @@
 let map;
 let mapview;
 let layer;
-let basemaps = [];
-// "streets", "satellite", "hybrid", "terrain", "topo", "gray", "dark-gray", "oceans", 
-// "national-geographic", "osm", "dark-gray-vector", "gray-vector", "streets-vector", 
-// "topo-vector", "streets-night-vector", "streets-relief-vector", "streets-navigation-vector"
-basemaps.push("satellite");
-basemaps.push("topo");
-basemaps.push("osm");
-basemaps.push("hybrid");
-basemaps.push("terrain");
-basemaps.push("dark-gray");
-let setBasemap = e => map.basemap = e.target.id;
-for (let i = 0; i < basemaps.length; i++) {
-    let buttons = document.getElementById("buttons");
-    let button = document.createElement("button");
-    button.id = basemaps[i];
-    button.textContent = basemaps[i];
-    button.addEventListener("click", setBasemap);
-    buttons.appendChild(button);
-}
+
 require([
     "esri/Map",
     "esri/views/MapView",
@@ -37,6 +19,7 @@ require([
     Legend,
     Expand
 ) {
+        generateBasemaps();
         map = new Map({ basemap: "streets" });
         let viewoptions = {
             container: "mapDiv",
@@ -56,41 +39,78 @@ require([
 
         let url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services?f=json";
         let options = { responseType: "json" };
-        Request(url, options).then(function (response) {
+        Request(url, options).then(populateMapService);
+
+        //populate the dropdown of mapService
+        function populateMapService(response) {
             let result = response.data;
             let lstservices = document.getElementById("lstservices");
-            lstservices.addEventListener("change", function () {
-                let selectedService = lstservices.options[lstservices.selectedIndex].textContent;
-                //add layer
-                layer = new MapLayer({ url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/" + selectedService + "/MapServer" });
-                map.removeAll();
-                map.add(layer);
-                layer.then(() => {
-                    let toc = document.getElementById("toc");
-                    toc.innerHTML="";
-                    for (let i = 0; i < layer.sublayers.length; i++) {
-                        let sublayer = layer.sublayers.items[i];
-                            let chk = document.createElement("input");
-                            chk.type = "checkbox";
-                            chk.value = sublayer.id;
-                            chk.checked = sublayer.visible;
-                            chk.addEventListener("click",function(e){
-                                let clickedlayer = layer.findSublayerById(parseInt(e.target.value));
-                                clickedlayer.visible = e.target.checked;
-                            })
-                            let lbl = document.createElement("lable");
-                            lbl.textContent = sublayer.title;
-                            toc.appendChild(chk);
-                            toc.appendChild(lbl);
-                    }
-                    mapview.goTo(layer.fullExtent)
-                })
-            })
+            lstservices.addEventListener("change", onChageServeiceMap);
 
             for (let i = 0; i < result.services.length; i++) {
                 let option = document.createElement("option");
                 option.textContent = result.services[i].name;
                 lstservices.appendChild(option);
             }
-        })
+        }
+
+        function onChageServeiceMap() {
+            let selectedService = lstservices.options[lstservices.selectedIndex].textContent;
+            //add layer
+            layer = new MapLayer({ url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/" + selectedService + "/MapServer" });
+            map.removeAll();
+            map.add(layer);
+            //wait until the layer is loaded.
+            layer.then(() => {
+                let toc = document.getElementById("toc");
+                toc.innerHTML = "";
+                let layerlist = document.createElement('ul');
+                toc.appendChild(layerlist);
+                populateLayer(layer, layerlist);
+                mapview.goTo(layer.fullExtent)
+            })
+        }
     });
+
+function populateLayer(thisLayer, layerlist) {
+    for (let i = 0; i < thisLayer.sublayers.length; i++) {
+        let sublayer = thisLayer.sublayers.items[i];
+        let chk = document.createElement("input");
+        chk.type = "checkbox";
+        chk.value = sublayer.id;
+        chk.checked = sublayer.visible;
+        chk.addEventListener("click", function (e) {
+            let clickedlayer = thisLayer.findSublayerById(parseInt(e.target.value));
+            clickedlayer.visible = e.target.checked;
+        })
+        let lbl = document.createElement("lable");
+        lbl.textContent = sublayer.title;
+
+        let layeritem = document.createElement("li");
+        layeritem.appendChild(chk);
+        layeritem.appendChild(lbl);
+        layerlist.appendChild(layeritem);
+    }
+}
+//generate list of buttons for basemaps
+function generateBasemaps() {
+    let basemaps = [];
+    // "streets", "satellite", "hybrid", "terrain", "topo", "gray", "dark-gray", "oceans", 
+    // "national-geographic", "osm", "dark-gray-vector", "gray-vector", "streets-vector", 
+    // "topo-vector", "streets-night-vector", "streets-relief-vector", "streets-navigation-vector"
+    basemaps.push("satellite");
+    basemaps.push("topo");
+    basemaps.push("osm");
+    basemaps.push("hybrid");
+    basemaps.push("terrain");
+    basemaps.push("dark-gray");
+    let setBasemap = e => map.basemap = e.target.id;
+    for (let i = 0; i < basemaps.length; i++) {
+        let buttons = document.getElementById("buttons");
+        let button = document.createElement("button");
+        button.id = basemaps[i];
+        button.textContent = basemaps[i];
+        button.addEventListener("click", setBasemap);
+        buttons.appendChild(button);
+    }
+}
